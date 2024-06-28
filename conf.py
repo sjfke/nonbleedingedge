4a79dfd1373541c38da7ade4620905326ee9f6dc
Sjfke <sjfke.pool.shark@hotmail.com>
2024-06-28 16:35:03 +0200

    version 2024.1.8

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ copyright = '2024, Geoffrey Collis'
 author = 'Geoffrey Collis (sjfke)'
 
 # The short X.Y version
-version = '2024.1.7'
+version = '2024.1.8'
 # The full version, including alpha/beta/rc tags
 release = ''
 
