c5a6d0a6790dc1b28aa808147f231dba96fbdd37
Sjfke <sjfke.pool.shark@hotmail.com>
2024-11-07 11:08:19 +0100

    bump version to 2024.11.07

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ copyright = '2024, Geoffrey Collis'
 author = 'Geoffrey Collis (sjfke)'
 
 # The short X.Y version
-version = '2024.11.7'
+version = '2024.11.07'
 # The full version, including alpha/beta/rc tags
 release = ''
 
